cc97debd78b38b1fb425ff275c47e3be49eee956
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-01-14 18:15:15 -0800

    Use template from before

---
+++ index.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+from datetime import date, datetime, timedelta
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+import sqlite3, calendar
+
+
+def main():
+    text = []
+    text.append('<!DOCTYPE html>')
+    text.append('<html>')
+    text.append('<title></title>')
+    text.append('<head>')
+    text.append('<meta charset=UTF-8>')
+    text.append('<style>')
+    text.append('body {font-family:sans-serif;font-size:15px;color:#222222}')
+    text.append('.mono {font-family:monospace}')
+    text.append('.caps {font-variant:small-caps}')
+    text.append('table {border-collapase:collapse}')
+    text.append('</style>')
+    text.append('</head>')
+    text.append('<body>')
+
+    text.append('</body>')
+    text.append('</html>')
+    return [line.encode('utf8') for line in text]
+
+
+def application(environ, start_response):
+    status = '200 OK'
+    
+    #output = 'Hello world'
+    output = main()
+    
+    response_headers = [('Content-type', 'text/html; charset=utf-8'),
+                        ('Content-Length', str(len('\n'.join(output))))]
+
+    start_response(status, response_headers)
+
+    return output
+
