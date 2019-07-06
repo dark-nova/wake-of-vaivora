733f9a7c52c37878c37c03626ebf80c63033a8be
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-07-05 20:04:07 -0700

    Use template wsgi.py from Digital Ocean (see comment in file)

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from myproject import app
+
+# code from https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-uswgi-and-nginx-on-ubuntu-18-04
+
+if __name__ == "__main__":
+    app.run()
